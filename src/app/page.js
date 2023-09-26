787aba813d12c1a4dc5d11e41aa8586d227b19be
Zulfikar Fajri <zulfikar.f@spacetoongo.com>
2023-09-26 16:30:04 +0700

    feat: update element styles

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,17 @@ import Map from "../components/Map";
 export default function Home() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="grid">
-        <div>
-          <p>Gansel Rechtsanwälte</p>
+      <div className="grid grid-cols-1 content-between">
+        <div className="h-fit">
+          <h3 className="mb-2 mt-0 text-3xl font-medium leading-tight text-primary">
+            Gansel Rechtsanwälte
+          </h3>
         </div>
-        <div className="grid">
-          <p>Adresse</p>
-          <p>Wallstraße 59, 10179 Berlin, Germany</p>
+        <div className="grid h-fit">
+          <p className="h-fit font-bold">Adresse</p>
+          <p className="h-fit text-neutral-900">
+            Wallstraße 59, 10179 Berlin, Germany
+          </p>
         </div>
       </div>
       <Map />
--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ import { Marker, GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "400px",
-  height: "400px",
+  height: "150px",
 };
 
 const center = {
